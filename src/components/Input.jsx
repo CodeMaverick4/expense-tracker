99f2451123bb8d955ae84dae6306d8d0d9a75ddf
Burhan Ahmed <burhan.ahmed@advantal.net>
2025-09-08 15:05:00 +0530

    apply all the valdiations.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,8 +2,8 @@ const Input = ({ value, type, placeholder, label, name, parentDivCss = "", onCha
     
     return (
         <div className={`input_container ${parentDivCss}`}>
+            <input type={type} name={name} value={value} onChange={onChange} placeholder={" "} />
             <span className="input_label_pos">{label}{required && <span className="required-star"> * </span>}</span>
-            <input type={type} name={name} value={value} onChange={onChange}  />
         </div>
     )
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,60 +1,83 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Input from "../components/Input";
 
 const Signup = () => {
   const [form, setForm] = useState({
-    name: "",
     email: "",
-    password: ""
+    password: "",
+    cnfPassword: ""
+  });
+  const [error, setError] = useState({
+    email: "",
+    password: "",
+    cnfPassword: ""
   });
 
   const handleChange = (e) => {
+    setError({ ...error, [e.target.name]: '' })
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log("claisjdlasjd")
+    if (form.email === '') {
+      setError(prev => ({ ...prev, email: "Please Enter email" }))
+      return
+    }
+    if (form.password === '') {
+      setError(prev => ({ ...prev, password: "Please Enter password" }))
+      return
+    }
+    if (form.cnfPassword === '') {
+      setError(prev => ({ ...prev, cnfPassword: "Please Enter Conform password" }))
+      return
+    }
     console.log("Form Submitted: ", form);
   };
+
 
   return (
     <div className="container d-flex justify-content-center align-items-center vh-100">
       <div className="col-md-4">
         <div className="card shadow p-4">
-          <h3 className="text-center mb-4">Sign Up</h3>
+          <h2 className="text-center mb-4 fw-bold">Sign Up</h2>
           <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-              <Input
-                label="Name"
-                name="name"
-                value={form.name}
-                onChange={handleChange}
-                placeholder="Enter your name"
-                type="text"
-                required
-              />
-            </div>
-            <div className="mb-3">
+            <div className="mb-4">
               <Input
                 label="Email"
                 name="email"
                 value={form.email}
                 onChange={handleChange}
-                placeholder="Enter your email"
-                type="email"
+                placeholder="Enter your name"
+                type="text"
                 required
               />
+              <p className="text-danger"><small>{error.email}</small></p>
             </div>
-            <div className="mb-3">
+            <div className="mb-4">
               <Input
                 label="Password"
                 name="password"
                 value={form.password}
                 onChange={handleChange}
+                placeholder="Enter your email"
+                type="password"
+                required
+              />
+              <p className="text-danger"><small>{error.password}</small></p>
+            </div>
+            <div className="mb-4">
+              <Input
+                label="Conform Password"
+                name="cnfPassword"
+                value={form.cnfPassword}
+                onChange={handleChange}
                 placeholder="Enter your password"
                 type="password"
                 required
               />
+              <p className="text-danger" ><small>{error.cnfPassword}</small></p>
             </div>
             <button type="submit" className="btn btn-primary w-100">
               Sign Up
